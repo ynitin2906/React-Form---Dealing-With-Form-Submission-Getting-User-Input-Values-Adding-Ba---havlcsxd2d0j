1063441415b7a1e06a54d245abf534c16aaab620
ynitin2906 <ynitin2906@gmail.com>
2023-03-09 01:45:02 +0530

    update2

--- src/utils/validation/Register.js
+++ src/utils/validation/Register.js
@@ -1,17 +1,21 @@
 export const validateForm = (formObject) => {
   const errors = {};
 
-  if (!formObject.username) {
+  if (!formObject.username.trim()) {
     errors.username = "Enter username";
   }
-  if (!formObject.email) {
+  if (!formObject.email.trim()) {
     errors.email = "Enter email";
   }
 
-  if (formObject.password.length < 4) {
+  if (!formObject.password.trim()) {
+    errors.password = "Password must have more than 4 characters";
+  } else if (formObject.password.length < 4) {
     errors.password = "Password must have more than 4 characters";
   }
-  if (formObject.contactNo.length !== 10) {
+  if (!formObject.contactNo.trim()) {
+    errors.contactNo = "Invalid contact no.";
+  } else if (formObject.contactNo.length !== 10) {
     errors.contactNo = "Invalid contact no.";
   }
   return errors;
