13e92f61081792a466226b6641b1eba1c0b9df47
ynitin2906 <ynitin2906@gmail.com>
2023-03-14 16:06:20 +0530

    updation

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,31 @@ const App = () => {
     password: "",
     contactNo: "",
   });
+  const validateForm = (formObject) => {
+    const errors = {};
+
+    if (!formObject.username) {
+      errors.username = "Username is required";
+    }
+
+    if (!formObject.email) {
+      errors.email = "Email is required";
+    }
+
+    if (!formObject.password) {
+      errors.password = "Password is required";
+    } else if (formObject.password.length < 4) {
+      errors.password = "Password must have more than 4 characters";
+    }
+
+    if (!formObject.contactNo) {
+      errors.contactNo = "Contact no. is required";
+    } else if (formObject.contactNo.length !== 10) {
+      errors.contactNo = "Invalid contact no.";
+    }
+
+    return errors;
+  };
 
   const onFormSubmit = (e) => {
     e.preventDefault();
